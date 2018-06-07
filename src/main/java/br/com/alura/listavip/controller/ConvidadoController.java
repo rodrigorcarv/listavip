5c07c057a7bb0f705151edc3a1e70df582f6541b
rodrigorcarv <rodrigorcarv@gmail.com>
2018-06-07 19:55:51 -0300

    alura2 - criando funcionalidade de salvar convidados

--- src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.com.alura.listavip.model.Convidado;
 import br.com.alura.listavip.repository.ConvidadoRepository;
 
 @Controller
@@ -18,11 +21,25 @@ public class ConvidadoController {
         return "index";
     }
 
-    @RequestMapping("listaconvidados")
+    @RequestMapping(value= "listaconvidados", method = RequestMethod.GET)
     public String listaConvidados(Model model) {
 
         model.addAttribute("convidados", convidadoRepository.findAll());
         return "listaconvidados";
     }
 
+    @RequestMapping(value= "salvar", method = RequestMethod.POST)
+    public String salvar(
+            @RequestParam("nome") String nome, 
+            @RequestParam("email") String email, 
+            @RequestParam("telefone") String telefone, Model model) {
+
+        Convidado convidado = new Convidado(nome, email, telefone);
+        convidadoRepository.save(convidado);
+
+        Iterable<Convidado> convidados = convidadoRepository.findAll();
+        model.addAttribute("convidados", convidados);
+
+        return "listaconvidados";
+    }
 }
--- src/main/java/br/com/alura/listavip/model/Convidado.java
+++ src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -14,6 +14,15 @@ public class Convidado {
     private String email;
     private String telefone;
 
+    public Convidado() {
+    }
+
+    public Convidado(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
     public Long getId() {
         return id;
     }
